1b8f2bfbdb6b6d0ec25b71b2d5c65cc2053b195c
umesh-rathore <umeshrathore897@gmail.com>
2026-09-05 22:15:30 +0530

    cleaned merge conflicts in buildVars.py

--- buildVars.py
+++ buildVars.py
@@ -5,13 +5,8 @@ addon_info = AddonInfo(
 	addon_name="TwoFactorAuthenticator",
 	addon_summary=_("2Factor Authenticator"),
 	addon_description=_("Generates 2FA TOTP codes quickly via a shortcut key and copies them to the clipboard."),
-<<<<<<< HEAD
-	addon_version="1.5",
-	addon_changelog=_("Added automatic QR code scanning from screen using opencv or wechat model to more compatibility with windows11 and nvda2026.2. no download needed, all libs are bundled. if you doubt libs are corrupted and not working? try: nvda menu > tools menu > 2fa menu and choose reinstall QRLibrary. the addon automatically reinstalls library and restarts nvda to continue."),
-=======
 	addon_version="1.5.2",
-	addon_changelog=_("Added automatic QR code scanning from screenusing opencv or wechat model to more compativility with windows11 and nvda2026.2. no download needed, all libs are bundled. if you dout libs are currupted and not working? try: nvda menu>tools menu> 2fa menu and choose reinstall QRLibrary. the addon automatic reinstall Library and restart nvda to continue."),
->>>>>>> a9672c3719147edb533811710d61d52ae0cc7f6d
+	addon_changelog=_("Added automatic QR code scanning from screen using opencv or wechat model to more compatibility with windows11 and nvda2026.2. no download needed, all libs are bundled. if you doubt libs are corrupted and not working? try: nvda menu > tools menu > 2fa menu and choose reinstall QRLibrary. the addon automatically reinstalls library and restarts nvda to continue."),
 	addon_author="Umesh Rathore <umeshrathore897@gmail.com>",
 	addon_url="https://github.com/umesh-rathore/2factor-authenticator",
 	addon_sourceURL="https://github.com/umesh-rathore/2factor-authenticator",
