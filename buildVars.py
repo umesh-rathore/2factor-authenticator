1b2db75f8e9bf92ab7d6fe01ab787443ec29b279
umesh-rathore <umeshrathore897@gmail.com>
2026-09-05 22:07:09 +0530

    fixed indentation error buildvars.py

--- buildVars.py
+++ buildVars.py
@@ -6,14 +6,15 @@ addon_info = AddonInfo(
 	addon_summary=_("2Factor Authenticator"),
 	addon_description=_("Generates 2FA TOTP codes quickly via a shortcut key and copies them to the clipboard."),
 	addon_version="1.5",
-	addon_changelog=_("Added automatic QR code scanning from screenusing opencv or wechat model to more compativility with windows11 and nvda2026.2. no download needed, all libs are bundled. if you dout libs are currupted and not working? try: nvda menu>tools menu> 2fa menu and choose reinstall QRLibrary. the addon automatic reinstall Library and restart nvda to continue."),
+	addon_changelog=_("Added automatic QR code scanning from screen using opencv or wechat model to more compatibility with windows11 and nvda2026.2. no download needed, all libs are bundled. if you doubt libs are corrupted and not working? try: nvda menu > tools menu > 2fa menu and choose reinstall QRLibrary. the addon automatically reinstalls library and restarts nvda to continue."),
 	addon_author="Umesh Rathore <umeshrathore897@gmail.com>",
 	addon_url="https://github.com/umesh-rathore/2factor-authenticator",
 	addon_sourceURL="https://github.com/umesh-rathore/2factor-authenticator",
 	addon_docFileName="readme.html",
 	addon_minimumNVDAVersion="2024.1",
 	addon_lastTestedNVDAVersion="2026.2",
-	addon_updateChannel=None,	
+	addon_updateChannel=None,
+)
 
 pythonSources: list[str] = [
 	"addon/globalPlugins/2factorAuthenticator/__init__.py",
